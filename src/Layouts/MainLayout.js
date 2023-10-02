3a6a7f25a87c97e9cf9f4651d5f4d4ebf83ad70c
Rayah Dwikat <rayah.hamdan95@gmail.com>
2023-10-02 22:22:35 +0300

    Update MainLayout.js

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { WelcomeSection } from "./WelcomeSection";
 import { Footer } from "./Footer";
+import { InputWithIcon } from "../components/InputWithIcon";
+
 
 export function MainLayout({ children }) {
     return (
@@ -10,6 +12,7 @@ export function MainLayout({ children }) {
 
             <Header />
             <WelcomeSection />
+            <InputWithIcon/>
             <main>
                 <Outlet />
             </main>
